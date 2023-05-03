7e0e55c878e35419197fb763c5f5eddc3c6c7089
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-05-03 11:59:11 +0000

    Modify animals table drop and add columns

--- schema.sql
+++ schema.sql
@@ -29,3 +29,11 @@ CREATE TABLE species(
   name VARCHAR(250),
   PRIMARY KEY(id)
 );
+
+-- Modify animals table
+-- Remove column species
+ALTER TABLE animals DROP species;
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals ADD species_id INT REFERENCES species(id);
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals ADD owner_id INT REFERENCES owners(id);
